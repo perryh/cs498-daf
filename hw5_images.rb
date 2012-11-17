35ba3da13e164eba7416403f53f4ad97787a8944
Perry Huang <huang32@llnl.gov>
2012-11-16 18:00:28 -0800

    added image downloader script

---
+++ hw5_images.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'google-search'
+
+iter = 0
+
+business_graphics = Google::Search::Image.new(:query => 'business graphic')
+
+business_graphics.each do |image|
+	if(iter > 100)
+		break
+	end
+	`wget #{image.uri} -P business_graphics`
+	iter = iter + 1
+end
+
+advertisements = Google::Search::Image.new(:query => 'advertisement')
+
+advertisements.each do |image|
+	if(iter > 100)
+		break
+	end
+	`wget #{image.uri} -P advertisements`
+	iter = iter + 1
+end
+
+scenes = Google::Search::Image.new(:query => 'scenes')
+
+scenes.each do |image|
+	if(iter > 100)
+		break
+	end
+	`wget #{image.uri} -P scenes`
+	iter = iter + 1
+end
